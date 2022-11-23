b9a206f8dacf5a2fa462e54f89e2b5207c436821
Sergey <sergastrah@yandex.ru>
2022-11-23 20:50:58 +0400

    add basename BrouserRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Layout from './components/Layout';
+
 import './App.scss';
 
 
@@ -15,11 +17,11 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
   )
 }
 
-export default App;
+export default App
